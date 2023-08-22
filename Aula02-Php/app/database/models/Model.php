8b200b99e845085222aeeba08d4ee65c8ac73ed1
Chriiis1421 <106244596+Chriiis1421@users.noreply.github.com>
2023-08-22 11:01:12 -0300

    Update Model.php

--- Aula02-Php/app/database/models/Model.php
+++ Aula02-Php/app/database/models/Model.php
@@ -2,7 +2,8 @@
     namespace app\database\models;
     use app\traits\Connection;
     use app\traits\Read;
+    use app\traits\Monitor;
 
     abstract class Model {
-        use Read, Connection;
+        use Read, Connection, Monitor;
     }
