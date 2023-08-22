9bae817f2e6910d70eae1c27af42200b698ecb0a
Chriiis1421 <106244596+Chriiis1421@users.noreply.github.com>
2023-08-22 12:15:57 -0300

    Add files via upload

---
+++ Aula02-Php/public/monitor.php
@@ -0,0 +1,16 @@
+<?php
+
+    ini_set('display_errors', 1); 
+    ini_set('display_startup_errors', 1); 
+    error_reporting(E_ALL);
+        
+    header("Acess-Control-Allow-Origin: *");
+    require __DIR__ . '/../vendor/autoload.php';
+
+    use app\controller\StudentController;
+
+    $obj = new StudentController();
+    
+    $data = $obj->monitor();
+    
+    print_r($data);
