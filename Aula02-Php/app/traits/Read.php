55565aaa724aa00efeccbc0cff54573f1a2993e6
Chriiis1421 <106244596+Chriiis1421@users.noreply.github.com>
2023-08-22 10:44:25 -0300

    Update Read.php

--- Aula02-Php/app/traits/Read.php
+++ Aula02-Php/app/traits/Read.php
@@ -6,7 +6,7 @@
 
         public function all($returnAll = true) {
             try {
-                $query - $this->connection->query("SELECT * FROM {$this->table}");
+                $query = $this->connection->query("SELECT * FROM {$this->table}");
 
                 return $returnAll ? $query->fetchAll() : $query->fetch();
 
